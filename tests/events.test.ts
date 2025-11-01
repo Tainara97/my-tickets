c1f2b3984beb2b7afd1c9c8ca5694e82ab1bafed
Tainara <tainara_rafaely@hotmail.com>
2025-11-01 11:11:01 -0300

    adiciona testes para POST/events e PUT/events/:id

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -1,7 +1,7 @@
 import prisma from "../src/database";
 import app from "index";
 import supertest from "supertest";
-import { createNewEvent } from "./factories/event-test";
+import { createNewEvent, createNewEventBody } from "./factories/event-test";
 
 const api = supertest(app);
 
@@ -50,6 +50,57 @@ describe("GET /events/:id", () => {
 
     })
 });
+
+describe("POST /events", () => {
+    it("should create a new event", async () => {
+        const data = createNewEventBody();
+        const { status } = await api.post("/events").send(data);
+        expect(status).toBe(201);
+    })
+
+    it("should return 409 when event name already exist", async () => {
+        const data = createNewEventBody();
+        await api.post("/events").send(data);
+
+        const { status, text } = await api.post("/events").send(data);
+
+        expect(status).toBe(409);
+        expect(text).toBe(`Event with name ${data.name} already registered.`);
+    })
+})
+
+describe("PUT /events/:id", () => {
+    it("should update an event", async () => {
+        const event = await createNewEvent();
+        const updateEvent = createNewEventBody();
+
+        const { status, body } = await api
+            .put(`/events/${event.id}`)
+            .send(updateEvent);
+
+        expect(status).toBe(200);
+        expect(body).toMatchObject({
+            id: event.id,
+            name: updateEvent.name,
+            date: updateEvent.date.toISOString()
+        })
+    })
+
+    it("should return 409 when event name already exist", async () => {
+        const event1 = await createNewEvent();
+        const event2 = await createNewEvent();
+
+        const { status, text } = await api
+            .put(`/events/${event2.id}`)
+            .send({
+                name: event1.name,
+                date: event2.date.toISOString()
+            })
+
+        expect(status).toBe(409);
+        expect(text).toBe(`Event with name ${event1.name} already registered.`);
+    })
+})
 
 afterAll(async () => {
     await prisma.$disconnect();
